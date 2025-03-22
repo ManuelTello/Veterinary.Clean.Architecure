9460d1efff5931c6442fb3a8237959865ab3168f
Manue Tello Siri <manuelTello.business@gmail.com>
2025-03-22 12:07:46 -0300

    Appointment added to the EF main application context

--- Source/NET.Veterinary.CA.Infrastructure/Persistence/Context/ApplicationContext.cs
+++ Source/NET.Veterinary.CA.Infrastructure/Persistence/Context/ApplicationContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using NET.Veterinary.CA.Domain.AggregateRoots;
 
 namespace NET.Veterinary.CA.Infrastructure.Persistence.Context
 {
     public class ApplicationContext:DbContext
     {
+        public DbSet<Appointment> Appointments { get; set; } 
+        
         public ApplicationContext(DbContextOptions<ApplicationContext> options):base(options){}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
